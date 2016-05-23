cfc17e722948710c298d5ab15fbc06fd7cd24fa7
Vytenis Bivainis <monrealis@gmail.com>
2016-05-23 11:57:09 +0300

    Exit with nonzero status if failed.

--- qUnit/testConfig.js
+++ qUnit/testConfig.js
@@ -3,4 +3,9 @@ var testrunner = require("qunit");
 testrunner.run({
     code: "math.js",
     tests: "tests\\math_tests.js"
-});
+},
+function(err, stats) {
+    process.exit(stats.failed > 0 ? 1 : 0);
+}
+);
+
