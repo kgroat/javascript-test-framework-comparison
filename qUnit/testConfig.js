f75e8c7cf553c7899e46fa48ea68abf0014556c8
Vytenis Bivainis <monrealis@gmail.com>
2016-05-24 00:25:22 +0300

    Change bakslashes to slashes.

--- qUnit/testConfig.js
+++ qUnit/testConfig.js
@@ -2,7 +2,7 @@ var testrunner = require("qunit");
 
 testrunner.run({
     code: "math.js",
-    tests: "tests\\math_tests.js"
+    tests: "tests/math_tests.js"
 },
 function(err, stats) {
     process.exit(stats.failed > 0 ? 1 : 0);
